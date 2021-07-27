d76137a6b5af4f217c2281a763cab60594dac07a
holyspark <313304981@qq.com>
2021-07-27 19:46:10 +0800

    Create s.py

--- nobook/s.py
+++ nobook/s.py
@@ -4,7 +4,7 @@
 import urllib.request
 import sys
 
-type = sys.getfilesystemencoding()
+# type = sys.getfilesystemencoding()
 def run():
 	url = "https://forum.nobook.com"
 	headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
@@ -13,9 +13,9 @@ def run():
 		data = urllib.request.urlopen(req).read()
 	except Exception as e:
 		return 0
-	html = data.decode("UTF-8").encode(type)
+	html = data.decode("UTF-8")
 
-	result=html.split(r'target="_blank" class="xi2">'.encode())
+	result=html.split(r'target="_blank" class="xi2">')
 	index = result[1].find('<')
 	user = result[1][0:index]
 	with open("users.txt", 'w', encoding="utf-8") as f:
